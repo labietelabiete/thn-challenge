e573dfe56324cf9a528ce96e3768dac7f3b32be8
labietelabiete <victorhugogote@gmail.com>
2021-11-19 17:53:58 +0100

    Feat: Getting date info

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  // Using headless we open the browser
-  const browser = await puppeteer.launch({ headless: false });
+  // const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
+
   const page = await browser.newPage();
 
-  /*LE PETIT ERMITAGE*/
   try {
     await page.goto(
       "https://reservations.travelclick.com/110426?datein=01%2F23%2F2022&dateout=01%2F30%2F2022&identifier=&_ga=2.265002822.859581762.1637252285-1760219410.1637252285#/accommodation/room"
@@ -15,59 +15,97 @@ const puppeteer = require("puppeteer");
 
     console.log("Container loaded");
 
-    const prices = await page.evaluate(() => {
-      const rooms = [];
-      const titles = document.querySelectorAll(".CardList-summary-title");
-      const prices = document.querySelectorAll(".CardList-price-title");
+    const allRooms = await page.evaluate(() => {
       let language = document.querySelectorAll(
         "#international-language-dropdown-id div span"
       )[0].innerHTML;
+      // Formatting language into two letters code
+      language = language.substring(0, 2).toUpperCase();
 
-      const currency = document.querySelector(
+      let currency = document.querySelector(
         "#international-language-dropdown-id div"
       ).textContent;
-      var firstDotPos = currency.indexOf("(");
-      var secondDotPos = currency.indexOf(")");
-      var myCurrency = currency.substring(firstDotPos + 1, secondDotPos);
+      currency = currency.substring(
+        currency.indexOf("(") + 1,
+        currency.indexOf(")")
+      );
 
       const guests = document.querySelectorAll(".HeaderButton-value div")[1]
         .innerHTML;
+      const adults = parseInt(guests.substring(0, 1));
+      const children = parseInt(guests.substring(2, 3));
+      const nGuests = adults + children;
 
-      const adults = guests.substring(0, 1);
-      const children = guests.substring(2, 3);
+      // Setting up information for all the rooms avaiables
+      const rooms = [];
+      const roomTitles = document.querySelectorAll(".CardList-summary-title");
+      const roomPrices = document.querySelectorAll(".CardList-price-title");
+      const nRooms = roomTitles.length;
+      // Others details from all the rooms. It contains an array with each room information
+      const allDetails = document.querySelectorAll(
+        "ul.CardList-summary-offerings"
+      );
 
-      for (let i = 0; i < titles.length; i++) {
+      for (let i = 0; i < nRooms; i++) {
         let room = {};
-        room.title = titles[i].innerHTML;
-        room.price = prices[i].innerHTML;
+        room.title = roomTitles[i].innerHTML;
+        room.price = roomPrices[i * 2].innerHTML;
+        room.others = [];
+
+        for (let j = 0; j < allDetails[i].children.length; j++) {
+          // For each room "i" we set an array with its corresponding detail "j"
+
+          // It is necessary to format every single data we get from the DOM
+          const numberDetail = parseInt(
+            allDetails[i].children[j].querySelector("span .IconLabel-label")
+              .textContent
+          ).toString();
+          const nameDetail = allDetails[i].children[j].querySelectorAll(
+            "span .IconLabel-label span"
+          )[1].textContent;
+
+          const detail = numberDetail + " " + nameDetail;
+          room.others.push(detail);
+        }
 
         rooms.push(room);
       }
 
-      return [adults, children, myCurrency, language, rooms];
+      // Building an array with the prices in number type to calculate the min price
+      const numbRoomPrices = [];
+
+      roomPrices.forEach((room) => {
+        let roomPriceValue = Number(
+          room.innerHTML.substring(1, room.innerHTML.length)
+        );
+        numbRoomPrices.push(roomPriceValue);
+      });
+
+      const minPrice = Math.min(...numbRoomPrices);
+
+      // Getting date
+      const dates = document.querySelector(".confirmation-info").textContent;
+
+      return [rooms, dates];
     });
 
-    console.log(prices);
-    // console.log(prices.length);
+    console.log(allRooms);
   } catch (error) {
-    console.log("The page couldn't be loaded", error);
+    console.log("The page couldn't be loaded, please check the url");
   }
+
+  await browser.close();
 
   /************************
    * checking date yyyy-mm-dd
    * checkout date yyyy-mm-dd
-   * minimum price (per night)
-   * currency of price (3 ISO char)
-   * number of rooms searched
-   * number of guests (adults and children)
-   * total number of guests
-   * language
+   * minimum price (per night) -------------
+   * currency of price (3 ISO char) -----------------
+   * number of rooms searched ------------
+   * number of guests (adults and children) -----------
+   * total number of guests ------------
+   * language ----------------
    * Data from minimum price (refundable, breakfast)
-   * Rates object with details
-   * Global: .CardList
-   *    title: .CardList-summary-title
-   *    .CardList-summary-offering-icon-label
-   *    price: .CardList-price-title
+   * Rates object with details --------------
    ************************/
-
 })();
